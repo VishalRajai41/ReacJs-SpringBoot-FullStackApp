b32fa0fe3956abf363f197064c6d98a756359b69
Vishal Rajai <55343199+VishalRajai41@users.noreply.github.com>
2021-05-13 22:17:27 +0530

    Employee Management Using Frontend Backend Spring Boot and RestApi

---
+++ ReactEMSpring/src/main/java/com/reactemspring/ReactEmSpringApplication.java
@@ -0,0 +1,13 @@
+package com.reactemspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactEmSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactEmSpringApplication.class, args);
+	}
+
+}
---
+++ ReactEMSpring/src/test/java/com/reactemspring/ReactEmSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactemspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactEmSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
